d97ef31ca72be33dd1680fb566b735461ad9e85c
Radwan <tara02664@gmail.com>
2024-03-22 20:42:55 +0200

    API_OCR_CBC_Report✨

---
+++ ocrapp/urls.py
@@ -0,0 +1,8 @@
+# ocr/urls.py
+
+from django.urls import path
+from .views import OCRAPIView
+
+urlpatterns = [
+    path('ocr/', OCRAPIView.as_view(), name='ocr_api'),
+]
---
+++ ocrapp/views.py
@@ -0,0 +1,59 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+import pytesseract
+import cv2
+import numpy as np
+import re
+import io
+
+class OCRAPIView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    def post(self, request, *args, **kwargs):
+        uploaded_image = request.FILES.get('image')
+        if uploaded_image:
+            image_data = uploaded_image.read() 
+            img_np_array = np.frombuffer(image_data, np.uint8) 
+            img = cv2.imdecode(img_np_array, cv2.IMREAD_COLOR)  
+            extracted_text = pytesseract.image_to_string(img)
+            extracted_details = preprocess_details(extracted_text)
+            return Response({'details': extracted_details})
+        else:
+            return Response({'error': 'No Image Find.'}, status=400)
+
+# def preprocess_image(img):
+#     # Implement your image preprocessing logic here
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     _, thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+#     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+#     processed_img = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
+#     return processed_img
+
+def preprocess_details(text):
+    ranges = {
+        'WBC': (4.12, 11.12),
+        'RBC': (4.40, 6.12),
+        'Platelet Count': (150, 400),
+        'Hemoglobin': (13.12, 18.12),
+    }
+    patterns = {
+        'WBC': r'WBC\s*Count\s*([\d.]+)',
+        'RBC': r'RBC\s*Count\s*([\d.]+)',
+        'Platelet Count': r'Platelet\s*Count\s*([\d.]+)',
+        'Hemoglobin': r'Hemoglobin\s*([\d.]+)',
+    }
+    extracted_details = {}
+    status_patient = "Good"
+    for key, pattern in patterns.items():
+        match = re.search(pattern, text, re.IGNORECASE)
+        if match:
+            value = float(match.group(1))
+            extracted_details[key] = value
+            if key in ranges:
+                if value < ranges[key][0] or value > ranges[key][1]:
+                    status_patient = "Not Good"
+        else:
+            extracted_details[key] = None
+            status_patient = "Not Good"
+
+    return {'status_patient': status_patient, 'details_OCR_CBC': extracted_details}
